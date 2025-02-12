60459664ad297b495422faa5428bf7868f781798
olundemo <bjornoskarstromberg@gmail.com>
2025-02-12 22:31:34 +0100

    No code yet 😅
    Created the ERD-diagram that can be found in the documentation directory. Then I started building out the models in the schema.prisma and migrating them to the db.

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Proxies /api requests to your backend
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      },
     },
   },
 })
