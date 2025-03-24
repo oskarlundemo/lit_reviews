87dcdb9d530510da1860fc4433422c57c38fca2b
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-03-24 19:31:40 +0100

    Proxy server changes

--- client/public-app/vite.config.js
+++ client/public-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'https://let-reviews-back-end.onrender.com', // Use the deployed backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
