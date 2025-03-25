c3c51051d1fcd5f9e9db98a4a17f2a5d0b73fbe5
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-03-25 18:06:20 +0100

    Fixing router and routes

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.use('/activity', activityRouter);
 
 
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/public-app/dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
