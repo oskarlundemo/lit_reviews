b64ed687d99e6e7ec4b83cdd35c17badea784930
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-03-25 17:46:00 +0100

    ES6 Module update

--- server/app.js
+++ server/app.js
@@ -44,11 +44,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.listen(PORT, () => {
