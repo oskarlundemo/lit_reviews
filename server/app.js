c31de67bf99da88aa8e56ab6e289082d6c14d110
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-03-25 17:51:52 +0100

    Fixing router and routes

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 5001;
 
 const app = express();
@@ -40,16 +41,11 @@ app.use('/comments', commentsRoute);
 app.use('/activity', activityRouter);
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '..client/dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
