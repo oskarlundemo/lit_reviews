7d936988880146ca0ff5259f2303f35e03132858
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-03-25 09:13:19 +0100

    Proxy server changes

--- client/public-app/vite.config.js
+++ client/public-app/vite.config.js
@@ -7,11 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'https://let-reviews-back-end.onrender.com', // Backend deployed URL
         changeOrigin: true,
-        secure: false, // If your backend uses HTTPS but with a self-signed certificate
-        rewrite: (path) => path.replace(/^\//, ''), // Remove leading slash from any request
+        rewrite: (path) => path.replace(/^\/api\//, '/'), // Ensure /api is removed properly
       },
     },
   },
